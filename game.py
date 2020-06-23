77a60e0717a328417718a69bbc0c9e9d7c018e31
Gael <gael.vanderlee@gmail.com>
2020-06-23 00:01:14 -0700

    Randomly generated levels and bug fixes

--- game.py
+++ game.py
@@ -2,10 +2,22 @@ import pygame as pg
 from pathlib import Path
 import math
 from pygame.locals import Color
+import random
+
+
+def generate_level(width=20, length=100):
+    level = list()
+    for i in range(length):
+        left = random.randint(0, width / 2)
+        right = random.randint(0, width / 2)
+        remainder = width - left - right
+        level.append("G" * left + " " * remainder + "G" * right)
+    return level
 
 
 def load_image(file, scale=None):
-    """ loads an image, prepares it for play
+    """
+    Loads an image, prepares it for play
     """
     fpath = SPRITES_DIR / file
     assert fpath.exists()
@@ -24,29 +36,30 @@ class Player(pg.sprite.Sprite):
     angle = 90
     speed = 0
     speed_x = 0
-    car_size = (50, 40)
+    car_size = (80, 60)
     max_speed = 10
 
     def __init__(self, terrain):
         super().__init__()
         self.original_image = load_image("car.png", self.car_size)
         self.image = pg.transform.rotate(self.original_image, self.angle)
-        self.rect = self.image.get_rect(midbottom=SCREEN_RECT.midbottom)
+        self.rect = self.image.get_rect(midbottom=(level_width / 2, level_height - TILE_SIZE))
         self.terrain = terrain
 
-    def speed_curve(self, x):
-        return self.max_speed * 2 * ((1 / (1 + math.exp(-x / 10))) - 0.5)
+    def speed_curve(self):
+        # Use a sigmoid as a car speed function
+        return self.max_speed * 2 * ((1 / (1 + math.exp(-self.speed_x / 10))) - 0.5)
 
     def update(self):
         keystrokes = pg.key.get_pressed()
         if keystrokes[pg.K_UP]:
             if self.speed + 0.01 < self.max_speed:
                 self.speed_x += self.acceleration
-                self.speed = self.speed_curve(self.speed_x)
+                self.speed = self.speed_curve()
         if keystrokes[pg.K_DOWN]:
             if self.speed - 0.01 > -self.max_speed:
                 self.speed_x -= self.acceleration
-                self.speed = self.speed_curve(self.speed_x)
+                self.speed = self.speed_curve()
         if keystrokes[pg.K_RIGHT]:
             self.angle -= self.turn_deg
             if self.angle < -180:
@@ -63,31 +76,29 @@ class Player(pg.sprite.Sprite):
         # Move car
         self.rect.move_ip(self.speed * math.cos(math.radians(self.angle)),
                           -self.speed * math.sin(math.radians(self.angle)))
-        self.rect = self.rect.clamp(SCREEN_RECT)
+        self.rect = self.rect.clamp(pg.Rect(0, 0, level_width, level_height))
 
-    def collide(self, xvel, yvel, terrain):
-        for p in platforms:
+        self.collide()
+
+    def collide(self):
+        for p in self.terrain:
             if pg.sprite.collide_rect(self, p):
-                if isinstance(p, FinishBlock):
+                if isinstance(p, Finish):
                     pg.event.post(pg.event.Event(pg.QUIT))
-                if xvel > 0:
-                    self.rect.right = p.rect.left
-                if xvel < 0:
-                    self.rect.left = p.rect.right
-                if yvel > 0:
-                    self.rect.bottom = p.rect.top
-                    self.onGround = True
-                    self.yvel = 0
-                if yvel < 0:
-                    self.rect.top = p.rect.bottom
+                if isinstance(p, Grass):
+                    self.speed_x = max(3, self.speed_x - self.acceleration * 2)
+                    self.speed = self.speed_curve()
+                    return
 
 
 class CameraAwareLayeredUpdates(pg.sprite.LayeredUpdates):
+
     def __init__(self, target, world_size):
         super().__init__()
         self.target = target
         self.cam = pg.Vector2(0, 0)
         self.world_size = world_size
+        self.font = pg.font.SysFont('Times New Roman', 60)
         if self.target:
             self.add(target)
 
@@ -102,23 +113,23 @@ class CameraAwareLayeredUpdates(pg.sprite.LayeredUpdates):
 
     def draw(self, surface):
         spritedict = self.spritedict
-        surface_blit = surface.blit
         dirty = self.lostsprites
         self.lostsprites = []
-        dirty_append = dirty.append
-        init_rect = self._init_rect
-        for spr in self.sprites():
+        for spr in self.sprites()[::-1]:
             rec = spritedict[spr]
-            newrect = surface_blit(spr.image, spr.rect.move(self.cam))
-            if rec is init_rect:
-                dirty_append(newrect)
+            newrect = surface.blit(spr.image, spr.rect.move(self.cam))
+            if rec is self._init_rect:
+                dirty.append(newrect)
             else:
                 if newrect.colliderect(rec):
-                    dirty_append(newrect.union(rec))
+                    dirty.append(newrect.union(rec))
                 else:
-                    dirty_append(newrect)
-                    dirty_append(rec)
+                    dirty.append(newrect)
+                    dirty.append(rec)
             spritedict[spr] = newrect
+            if isinstance(spr, Player):
+                text = self.font.render(f"Speed : {spr.speed:.2f}", True, Color("red"))
+                surface.blit(text, SCREEN_RECT.topleft)
         return dirty
 
 
@@ -132,70 +143,40 @@ class Terrain(pg.sprite.Sprite):
 
 class Grass(Terrain):
     def __init__(self, pos, *groups):
-        super().__init__(Color("Green"), pos, *groups)
+        super().__init__(Color("PaleGreen"), pos, *groups)
 
 
-class FinishBlock(Terrain):
+class Finish(Terrain):
     def __init__(self, pos, *groups):
         super().__init__(Color("Black"), pos, *groups)
 
 
 SPRITES_DIR = Path(__file__).resolve().parent / "sprites"
 SCREEN_RECT = pg.Rect((0, 0, 800, 640))
-TILE_SIZE = 16
+TILE_SIZE = 32
+level = generate_level()
+level_width = len(level[0]) * TILE_SIZE
+level_height = len(level) * TILE_SIZE
 
 
 def main():
-    def show_text(text):
-        text = font.render(text, True, Color("red"))
-        display.blit(text, SCREEN_RECT.topleft)
 
     pg.init()
     display = pg.display.set_mode(SCREEN_RECT.size)
     pg.display.set_caption("Ultra Racr")
     clock = pg.time.Clock()
-    font = pg.font.SysFont('Times New Roman', 20)
-    level = [
-        "GGGGGGGGGGGGGGGGGFFFFFFFFFGGGGGGGGGGGGGGGGGG",
-        "G                                          G",
-        "G                                          G",
-        "G                                          G",
-        "G                                          G",
-        "G                                          G",
-        "G                                          G",
-        "G                                          G",
-        "G                                          G",
-        "G                                          G",
-        "G                                          G",
-        "G                                          G",
-        "G                                          G",
-        "G                                          G",
-        "G                                          G",
-        "G                                          G",
-        "G                                          G",
-        "G                                          G",
-        "G                                          G",
-        "G                                          G",
-        "G                                          G",
-        "G                                          G",
-        "G                                          G",
-        "G                                          G",
-        "G                                          G",
-        "G                                          G",
-        "G                                          G",
-        "G                                          G",
-    ]
+
 
     terrain = pg.sprite.Group()
-    player = Player()
-    entities = CameraAwareLayeredUpdates(player, pg.Rect(0, 0, len(level[0])*TILE_SIZE, len(level)*TILE_SIZE))
+    player = Player(terrain)
+    entities = CameraAwareLayeredUpdates(player, pg.Rect(0, 0, level_width, level_height))
     x = y = 0
     for row in level:
         for col in row:
             if col == "G":
                 Grass((x, y), terrain, entities)
             if col == "F":
-                FinishBlock((x, y), terrain, entities)
+                Finish((x, y), terrain, entities)
             x += TILE_SIZE
         y += TILE_SIZE
         x = 0
@@ -207,12 +188,9 @@ def main():
 
         entities.update()
 
-        display.fill(Color("White"))
+        display.fill(Color("Gray21"))
         entities.draw(display)
-        display.blit(player.image, player.rect)
         pg.display.update()
-
-        show_text(f"Speed : {player.speed:.2f} Current Angle : {player.angle:.2f} ")
 
         clock.tick(60)
 
