8eeb742235f879023c31188a6d79520cfedb0313
Gael <gael.vanderlee@gmail.com>
2020-06-22 17:36:44 -0700

    Rotation and acceleration curves

--- game.py
+++ game.py
@@ -1,11 +1,7 @@
 import pygame as pg
-from matplotlib.colors import to_rgb
 from pathlib import Path
 import math
-
-
-def get_color(color_name: str) -> tuple:
-    return tuple([val * 255 for val in to_rgb(color_name)])
+from pygame.locals import Color
 
 
 def load_image(file, scale=None):
@@ -14,12 +10,12 @@ def load_image(file, scale=None):
     fpath = sprites_folder / file
     assert fpath.exists()
     try:
-        surface = pg.image.load(fpath.as_posix()).convert_alpha()
+        surface = pg.image.load(fpath.as_posix())
         if scale:
             surface = pg.transform.scale(surface, scale)
     except pg.error:
         raise SystemExit('Could not load image "%s" %s' % (fpath, pg.get_error()))
-    return surface.convert()
+    return surface.convert_alpha()
 
 
 sprites_folder = Path(__file__).resolve().parent / "sprites"
@@ -30,46 +26,58 @@ display = pg.display.set_mode(display_rect.size)
 
 
 class Car(pg.sprite.Sprite):
-    speed = 0.1
-    cur_angle = 90
-    car_size = (40, 20)
+    acceleration = 0.1
+    turn_deg = 2
+    angle = 90
+    speed = 0
+    speed_x = 0
+    car_size = (50, 25)
+    max_speed = 10
 
     def __init__(self):
         super().__init__()
         self.original_image = load_image("car.png", self.car_size)
-        self.image = pg.transform.rotate(self.original_image, self.cur_angle)
+        self.image = pg.transform.rotate(self.original_image, self.angle)
         self.rect = self.image.get_rect(midbottom=display_rect.midbottom)
-        self.speed_vector = [0.000001, 0.]
+
+    def speed_curve(self, x):
+        return self.max_speed * 2 * ((1 / (1 + math.exp(-x/10))) - 0.5)
 
     def move(self, keystrokes):
         if keystrokes[pg.K_UP]:
-            self.speed_vector[1] -= self.speed
+            if self.speed + 0.01 < self.max_speed:
+                self.speed_x += self.acceleration
+                self.speed = self.speed_curve(self.speed_x)
         if keystrokes[pg.K_DOWN]:
-            self.speed_vector[1] += self.speed
+            if self.speed - 0.01 > -self.max_speed:
+                self.speed_x -= self.acceleration
+                self.speed = self.speed_curve(self.speed_x)
         if keystrokes[pg.K_RIGHT]:
-            self.speed_vector[0] += self.speed
+            self.angle -= self.turn_deg
+            if self.angle < -180:
+                self.angle += 360
         if keystrokes[pg.K_LEFT]:
-            self.speed_vector[0] -= self.speed
+            self.angle += self.turn_deg
+            if self.angle > 180:
+                self.angle -= 360
 
     def draw(self, surface):
-        if abs(self.speed_vector[0]) + abs(self.speed_vector[1]) > 1:
-            speed_vec_angle = (180 / math.pi) * math.atan2(-self.speed_vector[1], self.speed_vector[0])
-            if abs(self.cur_angle - speed_vec_angle) > 0.01:
-                print(f"Changing angle from {self.cur_angle:.2f} to {speed_vec_angle:.2f}")
-                self.cur_angle = speed_vec_angle
-                self.image = pg.transform.rotate(self.original_image, speed_vec_angle)
-                self.rect = self.image.get_rect(topleft=self.rect.topleft)
+        self.image = pg.transform.rotate(self.original_image, self.angle)
+        self.rect = self.image.get_rect(topleft=self.rect.topleft)
 
-        self.rect.move_ip(*self.speed_vector)
+        self.rect.move_ip(self.speed * math.cos(math.radians(self.angle)),
+                          -self.speed * math.sin(math.radians(self.angle)))
         surface.blit(self.image, self.rect)
-        pg.draw.rect(display, get_color("red"), self.rect, 1)
         self.rect = self.rect.clamp(display_rect)
+
+    def is_collided_with(self, other):
+        self.rect.colliderect(other.rect)
 
 
 def main():
     player = Car()
     clock = pg.time.Clock()
-    # font = pg.font.SysFont('Times New Roman', 30)
+    font = pg.font.SysFont('Times New Roman', 30)
     while True:
 
         for event in pg.event.get():
@@ -78,15 +86,14 @@ def main():
         pressed_keys = pg.key.get_pressed()
         player.move(pressed_keys)
         player.draw(display)
-        # text = font.render(f"Speed : {player.speed_vector[0]:.2f}, {player.speed_vector[1]:.2f} "
-        #                    f"Current Angle : {player.cur_angle:.2f} "
-        #                    f"Speed angle : "
-        #                    f"{(180 / math.pi) * math.atan2(-player.speed_vector[1], player.speed_vector[0])}",
-        #                    True, get_color("red"))
-        #
-        # display.blit(text, display_rect.topleft)
+        text = font.render(f"Speed : {player.speed:.2f} "
+                           f"Current Angle : {player.angle:.2f} "
+                           f"FPS: {clock.get_fps()} ",
+                           True, Color("red"))
+
+        display.blit(text, display_rect.topleft)
         pg.display.flip()
-        display.fill(get_color("w"))
+        display.fill(Color("White"))
         clock.tick(60)
 
 
